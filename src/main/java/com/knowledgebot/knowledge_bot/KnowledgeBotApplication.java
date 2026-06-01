c955452af6e89fa4fb616126f5f6a0b61a949fb4
fshapouri <shapouri6f@gmail.com>
2026-06-01 13:05:45 +0330

    feat: initial project setup with docs folder and sample data

---
+++ src/main/java/com/knowledgebot/knowledge_bot/KnowledgeBotApplication.java
@@ -0,0 +1,13 @@
+package com.knowledgebot.knowledge_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeBotApplication.class, args);
+	}
+
+}

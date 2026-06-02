ae2c7490c80c81c2887a6dfcaeb341467a6016fb
fshapouri <shapouri6f@gmail.com>
2026-06-02 10:42:51 +0330

    feat: add KnowledgeController REST API endpoint

---
+++ src/main/java/com/knowledgebot/knowledge_bot/controller/KnowledgeController.java
@@ -0,0 +1,31 @@
+package com.knowledgebot.knowledge_bot.controller;
+
+import com.knowledgebot.knowledge_bot.service.AnswerService;
+import com.knowledgebot.knowledge_bot.service.DocumentRetriever;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class KnowledgeController {
+
+    private final AnswerService answerService;
+
+    public KnowledgeController(AnswerService answerService) {
+        this.answerService = answerService;
+    }
+
+    @GetMapping("/ask")
+    public Map<String, String> ask(@RequestParam String question) {
+        var answer =answerService.getAnswer(question.toLowerCase());
+        var response = new HashMap<String, String>();
+        response.put("question", question);
+        response.put("answer", answer);
+        return response;
+    }
+}
